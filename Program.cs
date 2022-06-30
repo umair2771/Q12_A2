67378d85ece774c3846ea6157846ba3672dc12dd
u2018340 <u2018340@giki.edu.pk>
2022-07-01 00:38:31 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Q12_A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Sentence ");
+            string line = Console.ReadLine();
+
+            Console.WriteLine("Original Sentence = " + line);
+
+            string updsen= line.Replace("ies", "s");
+            Console.WriteLine("Updated Sentence = " + updsen);
+        }
+    }
+}
